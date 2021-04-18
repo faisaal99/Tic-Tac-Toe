190acea412b34d686207122f6bd7026a821a483e
faisaal99 <faisal.163@hotmail.com>
2021-04-18 15:16:47 +0200

    major cleanup of code for checking winner
    instead of having 8 different functions that all kind of do the same thing, move it to one single function with array in parameter

--- scenes/play_logic.gd
+++ scenes/play_logic.gd
@@ -1,5 +1,4 @@
 extends Control
-
 class_name PlayLogic
 
 # All different combinations of solutions
@@ -20,22 +19,24 @@ export(Array, NodePath) var back_diagonal = []
 # Props
 var _circles = 0
 var _crosses = 0
+
+# Signals
 signal signal_end_game(player)
 signal switch_hud_player()
 
 # Check all rows, columns and diagonals
 # to determine if someone won
 func check_if_winner() -> bool:
-	var is_first_row = first_row_check()
-	var is_second_row = second_row_check()
-	var is_third_row = third_row_check()
+	var is_first_row = check_three(first_row)
+	var is_second_row = check_three(second_row)
+	var is_third_row = check_three(third_row)
 	
-	var is_first_col = first_col_check()
-	var is_second_col = second_col_check()
-	var is_third_col = third_col_check()
+	var is_first_col = check_three(first_column)
+	var is_second_col = check_three(second_column)
+	var is_third_col = check_three(third_column)
 	
-	var is_forw_dia = forw_check()
-	var is_back_dia = back_check()
+	var is_forw_dia = check_three(forw_diagonal)
+	var is_back_dia = check_three(back_diagonal)
 	
 	if is_first_row or is_second_row or is_third_row \
 	or is_first_col or is_second_col or is_third_col \
@@ -72,17 +73,13 @@ func clear_all():
 	
 	GameStatus.is_game_playing = true
 
-func first_row_check() -> bool:
-	var fr: Array = [
-		get_node(first_row[0]) as Square,
-		get_node(first_row[1]) as Square,
-		get_node(first_row[2]) as Square
-	]
+# Goes through an array of 3 squares and checks
+# if there are any three's
+func check_three(squares: Array):
+	var circles := 0
+	var crosses := 0
 	
-	var circles = 0
-	var crosses = 0
-	
-	for sq in fr:
+	for sq in [get_node(squares[0]), get_node(squares[1]), get_node(squares[2])]:
 		if sq.fill_status == Square.SquareStatus.CIRCLE:
 			circles += 1
 		elif sq.fill_status == Square.SquareStatus.CROSS:
@@ -90,117 +87,4 @@ func first_row_check() -> bool:
 	
 	if circles == 3 or crosses == 3:
 		return true
-	else:
-		return false
-func second_row_check() -> bool:
-	var sr: Array = [
-		get_node(second_row[0]) as Square,
-		get_node(second_row[1]) as Square,
-		get_node(second_row[2]) as Square
-	]
-	
-	var circles = 0
-	var crosses = 0
-	
-	for sq in sr:
-		if sq.fill_status == Square.SquareStatus.CIRCLE:
-			circles += 1
-		elif sq.fill_status == Square.SquareStatus.CROSS:
-			crosses += 1
-	
-	if circles == 3 or crosses == 3:
-		return true
-	else:
-		return false
-func third_row_check() -> bool:
-	var tr: Array = [
-		get_node(third_row[0]) as Square,
-		get_node(third_row[1]) as Square,
-		get_node(third_row[2]) as Square
-	]
-	
-	var circles = 0
-	var crosses = 0
-	
-	for sq in tr:
-		if sq.fill_status == Square.SquareStatus.CIRCLE:
-			circles += 1
-		elif sq.fill_status == Square.SquareStatus.CROSS:
-			crosses += 1
-	
-	if circles == 3 or crosses == 3:
-		return true
-	else:
-		return false
-
-func first_col_check() -> bool:
-	var circles = 0
-	var crosses = 0
-	
-	for sq in [get_node(first_column[0]), get_node(first_column[1]), get_node(first_column[2])]:
-		if sq.fill_status == Square.SquareStatus.CIRCLE:
-			circles += 1
-		elif sq.fill_status == Square.SquareStatus.CROSS:
-			crosses += 1
-	
-	if circles == 3 or crosses == 3:
-		return true
-	else:
-		return false
-func second_col_check() -> bool:
-	var circles = 0
-	var crosses = 0
-	
-	for sq in [get_node(second_column[0]), get_node(second_column[1]), get_node(second_column[2])]:
-		if sq.fill_status == Square.SquareStatus.CIRCLE:
-			circles += 1
-		elif sq.fill_status == Square.SquareStatus.CROSS:
-			crosses += 1
-	
-	if circles == 3 or crosses == 3:
-		return true
-	else:
-		return false
-func third_col_check() -> bool:
-	var circles = 0
-	var crosses = 0
-	
-	for sq in [get_node(third_column[0]), get_node(third_column[1]), get_node(third_column[2])]:
-		if sq.fill_status == Square.SquareStatus.CIRCLE:
-			circles += 1
-		elif sq.fill_status == Square.SquareStatus.CROSS:
-			crosses += 1
-	
-	if circles == 3 or crosses == 3:
-		return true
-	else:
-		return false
-
-func forw_check() -> bool:
-	var circles = 0
-	var crosses = 0
-	
-	for sq in [get_node(forw_diagonal[0]), get_node(forw_diagonal[1]), get_node(forw_diagonal[2])]:
-		if sq.fill_status == Square.SquareStatus.CIRCLE:
-			circles += 1
-		elif sq.fill_status == Square.SquareStatus.CROSS:
-			crosses += 1
-	
-	if circles == 3 or crosses == 3:
-		return true
-	else:
-		return false
-func back_check() -> bool:
-	var circles = 0
-	var crosses = 0
-	
-	for sq in [get_node(back_diagonal[0]), get_node(back_diagonal[1]), get_node(back_diagonal[2])]:
-		if sq.fill_status == Square.SquareStatus.CIRCLE:
-			circles += 1
-		elif sq.fill_status == Square.SquareStatus.CROSS:
-			crosses += 1
-	
-	if circles == 3 or crosses == 3:
-		return true
-	else:
-		return false
+	return false
